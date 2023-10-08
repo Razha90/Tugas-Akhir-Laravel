45f1d0214a27a0322694186a40943edf538295a9
razha90 <razhajamsiksyah@gmail.com>
2023-10-08 05:29:35 -0700

    1.5

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
+Route::post('/mahasiswa', [MahasiswaController::class, 'store']);
+Route::get('/mahasiswa/{id}', [MahasiswaController::class, 'show']);
+Route::put('/mahasiswa/{id}', [MahasiswaController::class, 'update']);
+Route::patch('/mahasiswa/{id}', [MahasiswaController::class, 'update']);
+Route::delete('/mahasiswa/{id}', [DosenController::class, 'destroy']);
 
 Route::get('/dosen', [DosenController::class, 'index']);
 Route::post('/dosen', [DosenController::class, 'store']);
@@ -32,6 +38,18 @@ Route::put('/dosen/{id}', [DosenController::class, 'update']);
 Route::patch('/dosen/{id}', [DosenController::class, 'update']);
 Route::delete('/dosen/{id}', [DosenController::class, 'destroy']);
 
-Route::apiResource('/dosen-pembimbing', DosenPembimbingController::class);
-Route::apiResource('/matakuliah', MatakuliahController::class);
-Route::apiResource('/matakuliah-mahasiswa', Matakuliah_MahasiswaController::class);
+Route::get('/dosen-pembimbing', [DosenPembimbingController::class, 'index']);
+Route::post('/dosen-pembimbing', [DosenPembimbingController::class, 'store']);
+Route::put('/dosen-pembimbing/{id}', [DosenPembimbingController::class, 'update']);
+Route::delete('/dosen-pembimbing/{id}', [DosenPembimbingController::class, 'destroy']);
+
+Route::get('/matakuliah', [MatakuliahController::class, 'index']);
+Route::post('/matakuliah', [MatakuliahController::class, 'store']);
+Route::put('/matakuliah/{id}', [MatakuliahController::class, 'update']);
+Route::delete('/matakuliah/{id}', [MatakuliahController::class, 'destroy']);
+Route::get('/matakuliah/{id}', [MatakuliahController::class, 'show']);
+
+Route::get('/matakuliah-mahasiswa', [Matakuliah_MahasiswaController::class, 'index']);
+Route::post('/matakuliah-mahasiswa', [Matakuliah_MahasiswaController::class, 'store']);
+Route::put('/matakuliah-mahasiswa/{id}', [Matakuliah_MahasiswaController::class, 'update']);
+Route::delete('/matakuliah-mahasiswa/{id}', [Matakuliah_MahasiswaController::class, 'destroy']);
--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
-Route::post('/mahasiswa', [MahasiswaController::class, 'store']);
-Route::get('/mahasiswa/{id}', [MahasiswaController::class, 'show']);
-Route::put('/mahasiswa/{id}', [MahasiswaController::class, 'update']);
-Route::patch('/mahasiswa/{id}', [MahasiswaController::class, 'update']);
-Route::delete('/mahasiswa/{id}', [MahasiswaController::class, 'destroy']);
+Route::get('/routes', function () {
+    $routeCollection = Route::getRoutes()->getRoutes();
+
+    return view('routes', ['routeCollection' => $routeCollection]);
+});
