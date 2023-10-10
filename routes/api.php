b6c43dd911113c0d2c626d499db3e4638a517850
razha90 <razhajamsiksyah@gmail.com>
2023-10-10 08:01:59 -0700

    v1

--- routes/api.php
+++ routes/api.php
@@ -23,28 +23,30 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/mahasiswa', [MahasiswaController::class, 'store']);
-Route::put('/mahasiswa/{id}', [MahasiswaController::class, 'update']);
-Route::patch('/mahasiswa/{id}', [MahasiswaController::class, 'update']);
-Route::delete('/mahasiswa/{id}', [DosenController::class, 'destroy']);
+Route::group(['prefix'=>'v1'],function () {
+    Route::post('/mahasiswa', [MahasiswaController::class, 'store']);
+    Route::put('/mahasiswa/{id}', [MahasiswaController::class, 'update']);
+    Route::patch('/mahasiswa/{id}', [MahasiswaController::class, 'update']);
+    Route::delete('/mahasiswa/{id}', [DosenController::class, 'destroy']);
 
-Route::post('/dosen', [DosenController::class, 'store']);
-Route::put('/dosen/{id}', [DosenController::class, 'update']);
-Route::patch('/dosen/{id}', [DosenController::class, 'update']);
-Route::delete('/dosen/{id}', [DosenController::class, 'destroy']);
+    Route::post('/dosen', [DosenController::class, 'store']);
+    Route::put('/dosen/{id}', [DosenController::class, 'update']);
+    Route::patch('/dosen/{id}', [DosenController::class, 'update']);
+    Route::delete('/dosen/{id}', [DosenController::class, 'destroy']);
 
-Route::post('/dosen-pembimbing', [DosenPembimbingController::class, 'store']);
-Route::put('/dosen-pembimbing/{id}', [DosenPembimbingController::class, 'update']);
-Route::delete('/dosen-pembimbing/{id}', [DosenPembimbingController::class, 'destroy']);
+    Route::post('/dosen-pembimbing', [DosenPembimbingController::class, 'store']);
+    Route::put('/dosen-pembimbing/{id}', [DosenPembimbingController::class, 'update']);
+    Route::delete('/dosen-pembimbing/{id}', [DosenPembimbingController::class, 'destroy']);
 
-Route::post('/matakuliah', [MatakuliahController::class, 'store']);
-Route::put('/matakuliah/{id}', [MatakuliahController::class, 'update']);
-Route::delete('/matakuliah/{id}', [MatakuliahController::class, 'destroy']);
-Route::get('/matakuliah/{id}', [MatakuliahController::class, 'show']);
+    Route::post('/matakuliah', [MatakuliahController::class, 'store']);
+    Route::put('/matakuliah/{id}', [MatakuliahController::class, 'update']);
+    Route::delete('/matakuliah/{id}', [MatakuliahController::class, 'destroy']);
+    Route::get('/matakuliah/{id}', [MatakuliahController::class, 'show']);
 
-Route::post('/matakuliah-mahasiswa', [Matakuliah_MahasiswaController::class, 'store']);
-Route::put('/matakuliah-mahasiswa/{id}', [Matakuliah_MahasiswaController::class, 'update']);
-Route::delete('/matakuliah-mahasiswa/{id}', [Matakuliah_MahasiswaController::class, 'destroy']);
+    Route::post('/matakuliah-mahasiswa', [Matakuliah_MahasiswaController::class, 'store']);
+    Route::put('/matakuliah-mahasiswa/{id}', [Matakuliah_MahasiswaController::class, 'update']);
+    Route::delete('/matakuliah-mahasiswa/{id}', [Matakuliah_MahasiswaController::class, 'destroy']);
+});
 
 Route::post('/test', function (Request $request) {
     $nama = $request->input('nama');
